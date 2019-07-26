56cc26a42925bb4076173913aa368da2b993c937
Goran Spasojevic <gospaso@gmail.com>
2019-07-26 11:29:03 -0400

    fix(*): clarify instructions, simplify code

--- index.js
+++ index.js
@@ -5,62 +5,30 @@ describe('object operations', () => {
   let data
 
   before(async () => {
-    /**
-     * INSTRUCTIONS:
-     *
-     * - Visit https://www.coindesk.com/api
-     * - You will find "Historical BPI data" at the bottom of the page with instructions on composing the query
-     * - Make a request for prices between January 1, 2019 - January 31, 2019
-     * - Populate the params to complete the request (you don't have to use the `url` package)
-     * - Attach the response to the data variable defined above
-     */
-    const params = null // TODO:
-
+    const params = null // See instructions in "Setting up"
     const result = await rp(params)
     data = result.bpi
   })
 
-  it('should receive correct data', () => {
-    /**
-     * This first test has been completed.
-     * It should pass if the query from step 1 is acurate.
-     */
+  it('should pass if the data has been correctly populated', () => {
     const exists = data.hasOwnProperty('2019-01-01')
     assert.ok(exists)
   })
 
-  it('should handle basic arithmetic', () => {
-    /**
-     * INSTRUCTIONS:
-     *
-     * Return the sum of all values
-     * See expected result below
-     */
-    const actual = null // TODO:
+  it('should return the sum of all values', () => {
+    const actual = null // TODO: #1
     const expected = 114390.02239999999
     assert.equal(actual, expected)
   })
 
-  it('should handle finding', () => {
-    /**
-     * INSTRUCTIONS:
-     *
-     * Find the top value (Number) from any date
-     * See expected result below
-     */
-    const actual = null // TODO:
+  it('should find the top value (Number) from any date', () => {
+    const actual = null // TODO: #2
     const expected = 4083.165
     assert.equal(actual, expected)
   })
 
-  it('should extract results', () => {
-    /**
-     * INSTRUCTIONS:
-     *
-     * Extract only the results from Mondays
-     * See expected result below
-     */
-    const actual = null // TODO:
+  it('should extract only the results from Mondays', () => {
+    const actual = null // TODO: #3
     const expected = {
       '2019-01-01': 3869.47,
       '2019-01-08': 4029.9917,
@@ -71,14 +39,8 @@ describe('object operations', () => {
     assert.deepEqual(actual, expected)
   })
 
-  it('should handle sorting', () => {
-    /**
-     * INSTRUCTIONS:
-     *
-     * Return the top 5 dates in descending order
-     * See expected result below
-     */
-    const actual = null // TODO:
+  it('should return the top 5 dates in descending order', () => {
+    const actual = null // TODO: #4
     const expected = {
       '2019-01-06': 4083.165,
       '2019-01-07': 4041.4583,
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ## Getting Started
 
-If you have yarn installed you can copy-paste the code below, otherwise replace with corresponding npm commands.
+If you are running a Unix system with yarn installed you can copy the code below, otherwise replace with corresponding Windows/npm commands.
 
 ```sh
 git clone https://github.com/coverse/js-test.git
@@ -13,15 +13,27 @@ The tests will run with `mocha --watch` and should immediately fail.
 
 Your task is to correct any errors and make all the tests pass.
 
-All the instruction are in the `index.js` file.
+## Instructions
+
+### Setting up
+
+  - Visit [https://www.coindesk.com/api](https://www.coindesk.com/api). Find "Historical BPI data" at the bottom of the page with instructions on composing the query
+  - Make a request for prices between January 1, 2019 - January 31, 2019
+  - Populate the params to complete the request
+
+Once you enter the correct parameters, the first test should pass.
+
+The following 4 tests each provide instructions and results required for solving them. They are as follows:
+- TODO #1 - Return the sum of all values
+- TODO #2 - Find the top value (Number) from any date
+- TODO #3 - Extract only the results from Mondays
+- TODO #4 - Return the top 5 dates in descending order
 
 ## Submitting the code
 
-Once you've completed the code and passed the tests, you have a few options:
-- Send us the `index.js` file as an attachment in your application
-- Send us a link to a Gist on GitHub with the contents of your `index` file
-- **Do not make a pull request to origin**
+Once you have completed the code and passed the tests, you need to send only the contents of your `index.js` file. You can either:
 
-Once we've received your submission, we will send you our own solution to the task for you to review.
+- upload to a service (like Gist, jsFiddle, Drive) and send us the link
+- copy the contents of the file in the body of your application, email, or chat
 
-If you are selected, we will contact you for a brief interview. You will likely begin with your first assignment right there and then.
+Once we've received your submission, we will send you our own solution to the task for you to review. If you are selected, we will contact you for a brief interview. You will likely begin with your first assignment right there and then.
